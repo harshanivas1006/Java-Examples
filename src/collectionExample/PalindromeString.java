63b400b1790cf434e9036d044e68fdaf799b8c65
harshanivas1006 <padavalanivas@gmail.com>
2025-06-12 18:31:52 +0530

    java

---
+++ src/collectionExample/PalindromeString.java
@@ -0,0 +1,46 @@
+
+//import java.util.Scanner;
+//public class PalindromeString {
+//	public static void main(String args[])
+//	{
+//		Scanner sc=new Scanner(System.in);
+//		String s=sc.nextLine();
+//		String t=" ";
+//		for(int i=0;i<s.length();i++)
+//		{
+//			t=s.charAt(i)+t;
+//			if(s.equalsIgnoreCase(t))
+//			{
+//				System.out.print("Palindrome");
+//			}
+//			else
+//			{
+//				System.out.print("Not Plaindrome");
+//				break;
+//			}
+//		}
+//	}
+//}
+
+package collectionExample;
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input string
+        System.out.print("Enter a string: ");  
+        String original = scanner.nextLine();
+
+        // Reverse the string
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        // Check if original and reversed are same
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+    }
+}
